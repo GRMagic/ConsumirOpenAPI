d8a836aa1e8e5f82c9bdfedd0bf1dca42c77bd15
GRMagic <gustavo.rm90@gmail.com>
2020-12-04 17:29:17 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsumirOpenAPI
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var httpClient = new System.Net.Http.HttpClient();
+            var api = new PetStoreClient(httpClient);
+            var pet = await api.GetPetByIdAsync(21221);
+            Console.WriteLine(pet.Category.Name);
+        }
+    }
+}
